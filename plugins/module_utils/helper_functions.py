5eadcaef3d9719675cf96f7d069a964cb4f7b6e5
Kenneth Cummings <kenneth@fliacummings.de>
2020-04-27 10:38:30 +0200

    add helper_functions

---
+++ plugins/module_utils/helper_functions.py
@@ -0,0 +1,22 @@
+import giteapy
+
+def _compare_dict_with_resource(input, resource):
+    change = False
+    updated_keys = []
+    for k, v in input.items():
+        if str(resource[k]) != str(v):
+            change = True
+            updated_keys.append(k)
+    if change:
+        return dict(change=change, updated_keys=updated_keys)
+    else:
+        return dict(change=change)
+
+
+def _connect_to_gitea(connection_params):
+    configuration = giteapy.Configuration()
+    configuration.host = connection_params.get('host')
+    configuration.username = connection_params.get('username')
+    configuration.password = connection_params.get('password')
+    api_instance = giteapy.AdminApi(giteapy.ApiClient(configuration))
+    return api_instance
