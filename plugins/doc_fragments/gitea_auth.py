679844e644e640ee230a0041e32fb25c27927dac
Kenneth Cummings <kenneth@fliacummings.de>
2020-05-04 22:14:30 +0200

    doc_fragment for gitea_auth

---
+++ plugins/doc_fragments/gitea_auth.py
@@ -0,0 +1,18 @@
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+
+class ModuleDocFragment(object):
+
+    DOCUMENTATION = r'''
+options:
+  gitea_host:
+    description: URL of the gitea API Endpoint
+    required: True
+  gitea_user:
+    description: gitea login user
+    required: True
+  gitea_password:
+    description: gitea login password
+    required: True
+'''
